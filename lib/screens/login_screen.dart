171665749ade3ff6e8a4894bb2a795e565d36bf5
Dwi Indah <dindahr30@gmail.com>
2024-11-30 10:51:29 +0700

    fix login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:final_project_2/screens/signup_screen.dart';
 import 'package:final_project_2/screens/home_screen.dart';
-<<<<<<< HEAD
 import 'package:final_project_2/screens/signup_screen.dart';
-=======
-import 'package:final_project_2/screens/guide_screen.dart';
->>>>>>> 92961b5cc6fbf0f1043bcefdcde132b9d61440fd
 import 'package:final_project_2/screens/forgot_password_screen.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -52,10 +47,6 @@ class LoginScreen extends StatelessWidget {
                 ),
               ),
               Expanded(
-<<<<<<< HEAD
-=======
-                child: InputForm(),
->>>>>>> 92961b5cc6fbf0f1043bcefdcde132b9d61440fd
                 flex: 5,
                 child: InputForm(),
               ),
@@ -134,7 +125,6 @@ class _InputFormState extends State<InputForm> {
 
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
     double screenWidth = MediaQuery.of(context).size.width;
 
     return Form(
@@ -147,80 +137,6 @@ class _InputFormState extends State<InputForm> {
           Container(
             decoration: BoxDecoration(
               color: const Color(0xFF80A4FF),
-=======
-    double screenwidth = MediaQuery.of(context).size.width;
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Container(
-          decoration: BoxDecoration(
-            color: Color(0xFF80A4FF),
-            border: Border.all(color: Color(0xFF80A4FF)),
-            borderRadius: BorderRadius.circular(10),
-          ),
-          width: screenwidth * 0.8,
-          child: Padding(
-            padding: const EdgeInsets.only(left: 10),
-            child: TextFormField(
-              controller: emailController,
-              decoration: InputDecoration(
-                labelText: 'Email',
-                border: InputBorder.none,
-                labelStyle: TextStyle(color: Colors.white, fontFamily: 'Poppins'),
-              ),
-            ),
-          ),
-        ),
-        SizedBox(height: 10),
-        Container(
-          decoration: BoxDecoration(
-            color: Color(0xFF80A4FF),
-            border: Border.all(color: Color(0xFF80A4FF)),
-            borderRadius: BorderRadius.circular(10),
-          ),
-          width: screenwidth * 0.8,
-          child: Padding(
-            padding: const EdgeInsets.only(left: 10),
-            child: TextFormField(
-              controller: passwordController,
-              decoration: InputDecoration(
-                labelText: 'Password',
-                border: InputBorder.none,
-                labelStyle: TextStyle(color: Colors.white, fontFamily: 'Poppins'),
-              ),
-            ),
-          ),
-        ),
-        const SizedBox(height: 10),
-        TextButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => const ForgotPasswordScreen()),
-            );
-          },
-          child: const Text(
-            "Forgot password?",
-            style: TextStyle(
-              color: Colors.white,
-              fontFamily: 'Poppins',
-              decoration: TextDecoration.underline,
-            ),
-          ),
-        ),
-        SizedBox(height: 20),
-        ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => HomeScreen()),
-            );
-          },
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Color.fromARGB(255, 199, 176, 81),
-            shape: RoundedRectangleBorder(
->>>>>>> 92961b5cc6fbf0f1043bcefdcde132b9d61440fd
               borderRadius: BorderRadius.circular(10),
             ),
             width: screenWidth * 0.8,
