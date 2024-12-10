6d4e65dac5adaea5c490c8485ab8ddba2e3ed49a
vopula <alkasidikprawira12@gmail.com>
2024-12-10 19:05:37 +0700

    new firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,30 @@ import 'package:final_project_2/screens/add_screen.dart';
 import 'package:final_project_2/screens/guide_screen.dart';
 import 'package:final_project_2/screens/home_screen.dart';
 import 'package:final_project_2/screens/profile_screen.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:final_project_2/models/wish_model.dart';
 import 'package:final_project_2/screens/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb) {
+     await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyDdbrK2GMJ3aIxuoJjSg2VWtvVy0tDLVDU",
+            authDomain: "tekberd4makeawish.firebaseapp.com",
+            projectId: "tekberd4makeawish",
+            storageBucket: "tekberd4makeawish.firebasestorage.app",
+            appId: "1:48801867552:web:82e0ec1a5c8a60b9ff1945",
+            messagingSenderId: "48801867552",
+            measurementId: "G-0FKBJLL3VF"));
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(
     ChangeNotifierProvider(
       create: (_) => WishModel(),
