854b849c4ec4532c6eb5efcc139722d0d3153173
arayzi <147472775+Arayzi@users.noreply.github.com>
2024-12-16 17:57:49 +0700

    Update reset_password.dart

--- lib/screens/reset_password.dart
+++ lib/screens/reset_password.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:final_project_2/screens/forgot_password_screen.dart';
 import 'package:final_project_2/screens/home_screen.dart';
+
 class ChangePasswordScreen extends StatefulWidget {
   const ChangePasswordScreen({super.key});
 
@@ -9,23 +10,19 @@ class ChangePasswordScreen extends StatefulWidget {
 }
 
 class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
-  final _currentPasswordController = TextEditingController();
   final _newPasswordController = TextEditingController();
   final _confirmPasswordController = TextEditingController();
 
-  bool _isCurrentPasswordVisible = false;
   bool _isNewPasswordVisible = false;
   bool _isConfirmPasswordVisible = false;
 
   @override
   void dispose() {
-    _currentPasswordController.dispose();
     _newPasswordController.dispose();
     _confirmPasswordController.dispose();
     super.dispose();
   }
 
-  // Validasi password: minimal 8 karakter, mengandung huruf besar, huruf kecil, dan angka
   bool _isPasswordValid(String password) {
     final regex = RegExp(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$');
     return regex.hasMatch(password);
@@ -46,7 +43,7 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
             color: Colors.yellow,
           ),
         ),
-        backgroundColor: Colors.blue[900],
+        backgroundColor: Colors.blue[900](citation_900),
         iconTheme: const IconThemeData(color: Colors.yellow),
       ),
       body: Padding(
@@ -54,31 +51,6 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              // Current Password Field
-              TextField(
-                controller: _currentPasswordController,
-                decoration: InputDecoration(
-                  labelText: 'Current Password',
-                  labelStyle: const TextStyle(fontFamily: 'Poppins'),
-                  suffixIcon: IconButton(
-                    icon: Icon(
-                      _isCurrentPasswordVisible
-                          ? Icons.visibility
-                          : Icons.visibility_off,
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        _isCurrentPasswordVisible = !_isCurrentPasswordVisible;
-                      });
-                    },
-                  ),
-                ),
-                obscureText: !_isCurrentPasswordVisible,
-                style: const TextStyle(fontFamily: 'Poppins'),
-              ),
-              const SizedBox(height: 16),
-
-              // New Password Field
               TextField(
                 controller: _newPasswordController,
                 decoration: InputDecoration(
@@ -101,8 +73,6 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
                 style: const TextStyle(fontFamily: 'Poppins'),
               ),
               const SizedBox(height: 16),
-
-              // Confirm Password Field
               TextField(
                 controller: _confirmPasswordController,
                 decoration: InputDecoration(
@@ -125,7 +95,6 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
                 style: const TextStyle(fontFamily: 'Poppins'),
               ),
               const SizedBox(height: 32),
-
               Align(
                 alignment: Alignment.centerRight,
                 child: GestureDetector(
@@ -146,20 +115,11 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
                   ),
                 ),
               ),
-
-              // Save Button
               ElevatedButton(
                 onPressed: () {
-                  final currentPassword =
-                      _currentPasswordController.text.trim();
                   final newPassword = _newPasswordController.text.trim();
-                  final confirmPassword =
-                      _confirmPasswordController.text.trim();
-
-                  // Validasi untuk memastikan kolom tidak kosong
-                  if (currentPassword.isEmpty ||
-                      newPassword.isEmpty ||
-                      confirmPassword.isEmpty) {
+                  final confirmPassword = _confirmPasswordController.text.trim();
+                  if (newPassword.isEmpty || confirmPassword.isEmpty) {
                     ScaffoldMessenger.of(context).showSnackBar(
                       const SnackBar(
                         content: Text(
@@ -168,18 +128,7 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
                         ),
                       ),
                     );
-                  } else if (!_isPasswordValid(currentPassword)) {
-                    // Validasi untuk currentPassword
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text(
-                          'Current password must be at least 8 characters long, contain uppercase, lowercase, and a number.',
-                          style: TextStyle(fontFamily: 'Poppins'),
-                        ),
-                      ),
-                    );
                   } else if (!_isPasswordValid(newPassword)) {
-                    // Validasi untuk newPassword
                     ScaffoldMessenger.of(context).showSnackBar(
                       const SnackBar(
                         content: Text(
@@ -189,24 +138,13 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
                       ),
                     );
                   } else if (!_isPasswordMatch(newPassword, confirmPassword)) {
-                    // Validasi untuk confirmPassword
                     ScaffoldMessenger.of(context).showSnackBar(
                       const SnackBar(
                         content: Text('Passwords do not match',
                             style: TextStyle(fontFamily: 'Poppins')),
                       ),
                     );
-                  } else if (newPassword == currentPassword) {
-                    // Validasi untuk memastikan password baru tidak sama dengan password lama
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text(
-                            'New password cannot be the same as the current password',
-                            style: TextStyle(fontFamily: 'Poppins')),
-                      ),
-                    );
                   } else {
-                    // Password valid dan cocok
                     ScaffoldMessenger.of(context).showSnackBar(
                       const SnackBar(
                           content: Text('Password successfully changed!',
